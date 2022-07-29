32476a69bf7fb327b3ef437e762cfff8884e21a4
danielchochlinski <danielchochlinski@hotmail.com>
2022-07-29 16:21:11 +0200

    server .js prod

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path");
 const colors = require("colors");
 const dotenv = require("dotenv").config({ path: __dirname + "../../.env" });
 const { errorHandler } = require("./middleware/errorMiddleware");
@@ -24,6 +24,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goals"));
 app.use("/api/users", require("./routes/users"));
 
+//serve client
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "..", "frontend", "build", "index.html")
+    );
+  });
+} else
+  app.get("/", (req, res) => {
+    res.send("Please set to production");
+  });
 app.use(errorHandler);
 
 app.listen(port, () => {
